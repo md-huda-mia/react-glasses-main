9bc09cba56f1da4f9ce27d9700cbb32bca8ce4ee
md-huda-mia <mdhudamia.dev@gmail.com>
2023-12-05 22:13:40 +0600

    complate-site

--- src/config/firebase.config.jsx
+++ src/config/firebase.config.jsx
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const firebaseConfig = {
-  apiKey: "AIzaSyDlZ43ds7mdMO6C8beArTTU5WND9Hw3F3c",
-  authDomain: "glasses-auth-e31f3.firebaseapp.com",
-  projectId: "glasses-auth-e31f3",
-  storageBucket: "glasses-auth-e31f3.appspot.com",
-  messagingSenderId: "939276939451",
-  appId: "1:939276939451:web:4305451e7f0747075ff99e",
-};
 
+const all = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: all.VITE_apiKey,
+  authDomain: all.VITE_authDomain,
+  projectId: all.VITE_projectId,
+  storageBucket: all.VITE_storageBucket,
+  messagingSenderId: all.VITE_messagingSenderId,
+  appId: all.VITE_appId,
+};
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
